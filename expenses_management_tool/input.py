1971ff29ade2e42d36909245c76e4278f687244a
till-teb <till.beruflich@hotmail.com>
2022-11-26 04:18:03 +0100

    good working input funktion

--- expenses_management_tool/input.py
+++ expenses_management_tool/input.py
@@ -1,15 +1,15 @@
-import pandas as pd
-from datetime import date
 import os
+from datetime import date
+import pandas as pd
 
 date = str(date.today())
 
 # get the right working directory
 root = os.getcwd()
-filename = 'expenses_dataset.csv'
+filename = "expenses_dataset.csv"
 
 # dataset
-file = os.path.join(root,filename)
+file = os.path.join(root, filename)
 
 
 # Execute query
@@ -17,63 +17,71 @@ def query(date):
     expense = str(input("what have you spent money on : ")).lower()
     amount = float(input("how much money have you spent : "))
     importance = int(input("how important was your expense? (1-4) : "))
-    category = int(input('''please choose the category:
+    category = int(
+        input(
+            """please choose the category:
 
                        (1) x
                        (2) x
                        (3) x
                        ...
-                       '''))
+                       """
+        )
+    )
 
-    df = pd.DataFrame({"expense": [expense],
-                       "amount":[amount] ,
-                        "category": [category],
-                       "importance": [importance],
-                       "date": [date]})
-    return(df)
+    df = pd.DataFrame(
+        {
+            "expense": [expense],
+            "amount": [amount],
+            "category": [category],
+            "importance": [importance],
+            "date": [date],
+        }
+    )
+    return df
+
 
 # store query in dataset (ds)
 def store(date, df):
-    '''
+    """
     check if a dataset already exists? 
     
     
             ---> If not, create one
             
             ---> If yes, save the query in the dataset. 
-    '''  
-      
+    """
+
     def store_in_new_ds(df):
-        '''
+        """
         stores the query-result in a new dataset.
         
-        '''
-        data = pd.DataFrame(columns= ["expense", "amount",  "importance", "category","date"]) 
+        """
+        data = pd.DataFrame(
+            columns=["expense", "amount", "importance", "category", "date"]
+        )
         frames = [df, data]
-        data = pd.concat( frames)
+        data = pd.concat(frames)
         data.to_csv("expenses_dataset.csv", index=False)
-        print('--saved--')
+        print("--saved--")
         print(data)
-        return data 
+        return data
 
-    # check if a dataset already exist 
+    # check if a dataset already exist
     try:
         data = pd.read_csv(file)
         frames = [df, data]
         data = pd.concat(frames)
         data.to_csv("expenses_dataset.csv", index=False)
-        print('--saved--')
+        print("--saved--")
         print(data)
-        return data 
-    
-    # if not, create one 
+        return data
+
+    # if not, create one
     except:
         store_in_new_ds(df)
         print('\n--->new dataset "expenses_dataset.csv"  was successfully created.\n')
-        
-        
 
 
-  
 if __name__ == "__main__":
     store(date, query(date))
