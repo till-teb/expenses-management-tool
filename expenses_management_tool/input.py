e9b0dae7ad67db1b49c75ea46f3ac75dcef7b172
till-teb <till.beruflich@hotmail.com>
2022-11-26 18:44:25 +0100

    cleaner code version now

--- expenses_management_tool/input.py
+++ expenses_management_tool/input.py
@@ -2,18 +2,18 @@ import os
 from datetime import date
 import pandas as pd
 
-date = str(date.today())
+DATE = str(date.today())
 
 # get the right working directory
 root = os.getcwd()
-filename = "expenses_dataset.csv"
+FILENAME = "expenses_dataset.csv"
 
 # dataset
-file = os.path.join(root, filename)
+file = os.path.join(root, FILENAME)
 
 
 # Execute query
-def query(date):
+def query(DATE):
     expense = str(input("what have you spent money on : ")).lower()
     amount = float(input("how much money have you spent : "))
     importance = int(input("how important was your expense? (1-4) : "))
@@ -35,18 +35,14 @@ def query(date):
             "amount": [amount],
             "category": [category],
             "importance": [importance],
-            "date": [date]
-            
-            
-            
-            ,
+            "DATE": [DATE],
         }
     )
     return df
 
 
 # store query in dataset (ds)
-def store(date, df):
+def store(DATE, df):
     """
     check if a dataset already exists? 
     
@@ -62,14 +58,14 @@ def store(date, df):
         
         """
         data = pd.DataFrame(
-            columns=["expense", "amount", "importance", "category", "date"]
+            columns=["expense", "amount", "importance", "category", "DATE"]
         )
         frames = [df, data]
         data = pd.concat(frames)
-        data = split_dates(data)  # day, month, year as new columns
+        data = split_DATE(data)  # day, month, year as new columns
         data.to_csv("expenses_dataset.csv", index=False)
         print("--saved--")
-        to_drop = ['date'] # only columns: day, month, year column are nessesary
+        to_drop = ["DATE"]  # only columns: day, month, year column are nessesary
         data = data.drop(to_drop, axis=1)
         print(data)
         return data
@@ -78,11 +74,11 @@ def store(date, df):
     try:
         data = pd.read_csv(file)
         frames = [df, data]
-        data = pd.concat(frames) 
-        split_dates(data) # day, month, year as new columns
+        data = pd.concat(frames)
+        split_DATE(data)  # day, month, year as new columns
         data.to_csv("expenses_dataset.csv", index=False)
         print("--saved--")
-        to_drop = ['date']  # only columns: day, month, year column are nessesary
+        to_drop = ["DATE"]  # only columns: day, month, year column are nessesary
         data = data.drop(to_drop, axis=1)
         print(data)
         return data
@@ -91,34 +87,20 @@ def store(date, df):
     except:
         store_in_new_ds(df)
         print('\n--->new dataset "expenses_dataset.csv"  was successfully created.\n')
-    
-    
-    
-    
+
+
 # useful functions: ( later outscourced to own file: "mainly_used_functions" )
-    
-    
-# split date into seperatet columns
-def split_dates(data):
-    #splits the date column in to three new_columns:
-    new_columns = data['date'].str.split('-', expand= True) 
-    data['day'] = new_columns[2]
-    data['month'] =new_columns[1]
-    data['year'] =new_columns[0]
+
+
+# split DATE into seperatet columns
+def split_DATE(data):
+    # splits the DATE column in to three new_columns:
+    new_columns = data["DATE"].str.split("-", expand=True)
+    data["day"] = new_columns[2]
+    data["month"] = new_columns[1]
+    data["year"] = new_columns[0]
     return data
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 
 if __name__ == "__main__":
-    store(date, query(date))
+    store(DATE, query(DATE))
