e1d626ebd050f7412eb84a2bb85c29275b13a694
l4nz8 <vin.bohm456@gmail.com>
2023-02-09 15:27:05 +0100

    first Dashboard plotly update

--- expenses_management_tool/pages/dashboard.py
+++ expenses_management_tool/pages/dashboard.py
@@ -1,60 +1,52 @@
+import os
 import streamlit as st
+import plotly.express as px
 import pandas as pd
-import matplotlib.pyplot as plt
 
+# Get the right working directory
+root = os.getcwd()
+datasets = "datasets"
+FILENAME = ["recurring_expenses.csv", "income_dataset.csv", "expenses_dataset.csv"]
 
 st.title("Dashboard")
 
-if "df" not in st.session_state:
-    st.write("Please upload your csv or enter your data to see your dashboard")
+# Create the dropdown list
+selected_file = st.sidebar.selectbox("Select your CSV file", FILENAME)
 
+for file in FILENAME:
+    try:
+        # Load the dataset
+        datasets_PATH = os.path.join(root, datasets, file)
+        st.session_state[file] = pd.read_csv(datasets_PATH)
+    except:
+        st.sidebar.write(f"No {file} file found")
 
-if "df" in st.session_state:
-    df = st.session_state["df"]
-    st.write(df)
-
-    category = df["category"].value_counts()
-
-
-
-    st.header("Spend by category")
-    fig, ax = plt.subplots(figsize=(5, 5))
-    ax.pie(
-        category,
-        labels=(category.index),
-        wedgeprops={"linewidth": 7, "edgecolor": "white"},
-    )
-    # display a white circle in the middle of the pie chart
-    p = plt.gcf()
-    p.gca().add_artist(plt.Circle((0, 0), 0.7, color="white"))
-    st.pyplot(fig)
-
-
-# Categories
-
-    category = df["importance"].value_counts()
-     
-     
-     
-    st.header("distribution of importance")
-    fig, ax = plt.subplots(figsize=(5, 5))
-    ax.pie(
-        category,
-        labels=(category.index),
-        wedgeprops={"linewidth": 7, "edgecolor": "white"},
-    )
-    # display a white circle in the middle of the pie chart
-    p = plt.gcf()
-    p.gca().add_artist(plt.Circle((0, 0), 0.7, color="white"))
-    st.pyplot(fig)
+if f"{selected_file}" in st.session_state:
+    inc_df = st.session_state["income_dataset.csv"]
+    recex_df = st.session_state["recurring_expenses.csv"]
+    ex_df = st.session_state["expenses_dataset.csv"]
     
-# amount plot
-
-    st.header("amount range")
-    barh_data = df["amount"]
-    fig, ax = plt.subplots()
-    ax.barh(barh_data,width=1)
-    st.bar_chart(barh_data)
-
+    # Financial overview bar plot
+    y1 = inc_df["amount"].sum()
+    y2 = recex_df["amount"].sum() + ex_df["amount"].sum()
     
-
+    df = pd.DataFrame({"Financial overview": [y2, y1], "Index": [2,1]})
+    
+    fig = px.bar(df, x="Financial overview", y="Index", 
+                 color=["expenses", "income"], 
+                 color_discrete_sequence=[px.colors.qualitative.Alphabet[1], px.colors.qualitative.Plotly[2]], 
+                 orientation="h", 
+                 barmode="stack")
+    
+    # Change size and width
+    fig.update_layout(width=800, height=200)
+    
+    st.plotly_chart(fig)
+    st.write(f"Total income : {y1}")
+    st.write(f"Total expenses : {y2}")
+    
+    # Print selected DF
+    st.write(st.session_state[f"{selected_file}"])
+    
+    
+    
