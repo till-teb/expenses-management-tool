1ab849e762c0439d634b5571edc15d1e4078458a
l4nz8 <vin.bohm456@gmail.com>
2023-02-09 17:46:14 +0100

    second Dashboard plotly update with time selection

--- expenses_management_tool/pages/dashboard.py
+++ expenses_management_tool/pages/dashboard.py
@@ -6,14 +6,28 @@ import pandas as pd
 # Get the right working directory
 root = os.getcwd()
 datasets = "datasets"
-FILENAME = ["recurring_expenses.csv", "income_dataset.csv", "expenses_dataset.csv"]
+FILENAME = { "expenses" : "expenses_dataset.csv", "recurring" : "recurring_expenses.csv", "income" : "income_dataset.csv"}
+
+months = {
+"Jan" : 1,
+"Feb" : 2,
+"Mar" : 3,
+"Apr" : 4,
+"May" : 5,
+"Jun" : 6,
+"Jul" : 7,
+"Aug" : 8,
+"Sep" : 9,
+"Oct" : 10,
+"Nov" : 11,
+"Dec" : 12}
 
 st.title("Dashboard")
 
 # Create the dropdown list
-selected_file = st.sidebar.selectbox("Select your CSV file", FILENAME)
+selected_file = st.sidebar.selectbox("Select your CSV file", FILENAME.keys())
 
-for file in FILENAME:
+for file in FILENAME.values():
     try:
         # Load the dataset
         datasets_PATH = os.path.join(root, datasets, file)
@@ -21,32 +35,53 @@ for file in FILENAME:
     except:
         st.sidebar.write(f"No {file} file found")
 
-if f"{selected_file}" in st.session_state:
+if f"{FILENAME[selected_file]}" in st.session_state:
+    
     inc_df = st.session_state["income_dataset.csv"]
     recex_df = st.session_state["recurring_expenses.csv"]
     ex_df = st.session_state["expenses_dataset.csv"]
     
-    # Financial overview bar plot
-    y1 = inc_df["amount"].sum()
-    y2 = recex_df["amount"].sum() + ex_df["amount"].sum()
+    # select time span
+    selected_month = st.sidebar.selectbox("Choose month", months.keys())
+    selected_year = st.sidebar.selectbox("Choose year", ex_df["year"].unique().tolist()[::-1])
     
-    df = pd.DataFrame({"Financial overview": [y2, y1], "Index": [2,1]})
+    def select_time_span():
+        
+        if selected_file == "recurring":
+            filtered_df = st.session_state[FILENAME[selected_file]]
+            st.write(filtered_df)
+        else:
+            mask = (st.session_state[FILENAME[selected_file]]["month"] == months[selected_month]) | (st.session_state[FILENAME[selected_file]]["month"].isna())
+            filtered_df = st.session_state[FILENAME[selected_file]][mask]
+            st.write(filtered_df)
+            
+    def Financial_overview():
+        
+        # Financial overview bar plot
+        y1 = inc_df["amount"].sum()
+        y2 = recex_df["amount"].sum() + ex_df["amount"].sum()
+        
+        df = pd.DataFrame({"Financial overview": [y2, y1], "Index": [2,1]})
+        
+        fig = px.bar(df, x="Financial overview", y="Index", 
+                     color=["expenses", "income"], 
+                     color_discrete_sequence=[px.colors.qualitative.Alphabet[1], px.colors.qualitative.Plotly[2]], 
+                     orientation="h", 
+                     barmode="stack")
+        
+        # Change size and width
+        fig.update_layout(width=800, height=200)
+        
+        st.plotly_chart(fig)
     
-    fig = px.bar(df, x="Financial overview", y="Index", 
-                 color=["expenses", "income"], 
-                 color_discrete_sequence=[px.colors.qualitative.Alphabet[1], px.colors.qualitative.Plotly[2]], 
-                 orientation="h", 
-                 barmode="stack")
+        st.write(f"Total income : {y1}")
+        st.write(f"Total expenses : {y2}")
     
-    # Change size and width
-    fig.update_layout(width=800, height=200)
-    
-    st.plotly_chart(fig)
-    st.write(f"Total income : {y1}")
-    st.write(f"Total expenses : {y2}")
     
     # Print selected DF
-    st.write(st.session_state[f"{selected_file}"])
+    Financial_overview()
+    select_time_span()
     
-    
+    #test
+    st.write(months[selected_month])
     
